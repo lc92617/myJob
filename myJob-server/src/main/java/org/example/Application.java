61c0a36fc13c5f22b2ef30a7f2d464b2b0707c06
liuchang <liuchang@xbmail.com.cn>
2023-04-21 17:34:41 +0800

    create job

--- myJob-server/src/main/java/org/example/Application.java
+++ myJob-server/src/main/java/org/example/Application.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
 @MapperScan(basePackages = "org.example.repository.**.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
